a04dbc826a1878ceb63ad5af218e9542644b6b0f
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-10-04 20:55:35 +0200

    C ucenje pocetak

---
+++ c-vezbe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int firstNumber = 11;
+  int secondNumber = 120;
+
+  float thirdNumber = 121221.33452;
+
+  char someChar = 'a';
+
+  printf("First value: %d \n", firstNumber);
+  printf("Second value: %d \n", secondNumber);
+  printf("Some Char value: %c \n", someChar);
+  printf("Third Num value: %f \n", thirdNumber);
+
+  return 0;
+}
