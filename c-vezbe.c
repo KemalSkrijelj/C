2f0b6559f8bd96486de949ea7264655ab7f6265b
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-10-06 18:37:35 +0200

    Constante i operacije

--- c-vezbe.c
+++ c-vezbe.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
+// #define STATICNUMBER 200  //nacin definisanje constate
+
+// const int staticNum = 500;
+
 int main()
 {
-  int firstNumber = 11;
-  int secondNumber = 120;
+  printf("#################### \n");
+  printf("\n");
+  printf("Dobro dosli u jednostavan calculator \n");
+  printf("\n");
+  printf("#################### \n");
 
-  float thirdNumber = 121221.33452;
+  int firstNum = 13;
+  int secondNum = 5;
 
-  char someChar = 'a';
+  int add = firstNum + secondNum;
+  int sub = firstNum - secondNum;
+  int mul = firstNum * secondNum;
+  int div = firstNum / secondNum;
+  int rem = firstNum % secondNum;
 
-  printf("First value: %d \n", firstNumber);
-  printf("Second value: %d \n", secondNumber);
-  printf("Some Char value: %c \n", someChar);
-  printf("Third Num value: %f \n", thirdNumber);
+  printf("Add: %d \n", add);
+  printf("Sub: %d \n", sub);
+  printf("Mul: %d \n", mul);
+  printf("Div: %d \n", div);
+  printf("Rem: %d \n", rem);
 
   return 0;
 }
