d4f3e38e927637551956bf5a41f5ce88fdb3f39d
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-10-09 19:41:59 +0200

    Break and continue

--- c-vezbe.c
+++ c-vezbe.c
@@ -7,13 +7,36 @@
 int main()
 {
   int x = 0;
-  do
-  {
-    printf("x in this moment of time: %d \n", x);
-    x++;
-  } while (x < 20);
   
-
+  while (x <= 10)
+  {
+    if (x == 7)
+    {
+      x++;
+      continue;
+    }
+    
+     printf("X je u ovom momentu broj - %d \n", x);
+     x++;
+  }
+  
+  // int x;
+  // for ( x = 0; x < 10; x++)
+  // {
+  //   if (x == 5)
+  //   {
+  //     continue;
+  //   }
+  //   printf("Ovo su brojevi %d \n", x);
+  // }
+  
+  // int x = 0;
+  // do
+  // {
+  //   printf("x in this moment of time: %d \n", x);
+  //   x++;
+  // } while (x < 20);
+  
   // int x = 0;
 
   // while (x < 10)
