3b6b791a46cfa534209662384f4fb19f099604cc
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2024-11-23 20:43:01 +0100

    Vezbe

--- funkcije.c
+++ funkcije.c
@@ -26,31 +26,41 @@ float kub(float a)
 
 int main()
 {
-  float a, b, x, z1, y, z2, z3;
+  int a;
   printf("Unesi a: \n");
-  scanf("%f", &a);
-  printf("Unesi b: \n");
-  scanf("%f", &b);
-  printf("Unesi x: \n");
-  scanf("%f", &x);
-  printf("Unesi y: \n");
-  scanf("%f", &y);
-  z1 = x + kvadrat(y);
-  printf("Z1 = %.2f \n", z1);
-  z2 = kub(x) - deljenje(x, y);
-  printf("Z2 = %.2f \n", z2);
-  z3 = proizvod(x,y) + oduzimanje(5, y);
-  printf("Z3 = %.2f \n", z3);
+  scanf("%d", &a);
+  int faktorijel = 1;
+  for (int i = 1; i <= a; i++)
+  {
+    faktorijel *= i;
+  }
+  printf("Faktorijel: %d", faktorijel);
   
-  float proizvod_funkcija = proizvod(a, b);
-  float zbir_funkcija = zbir(a, b);
-  float deljenje_funkcija = deljenje(a, b);
-  float oduzimanje_funkcija = oduzimanje(a, b);
+  // float a, b, x, z1, y, z2, z3;
+  // printf("Unesi a: \n");
+  // scanf("%f", &a);
+  // printf("Unesi b: \n");
+  // scanf("%f", &b);
+  // printf("Unesi x: \n");
+  // scanf("%f", &x);
+  // printf("Unesi y: \n");
+  // scanf("%f", &y);
+  // z1 = x + kvadrat(y);
+  // printf("Z1 = %.2f \n", z1);
+  // z2 = kub(x) - deljenje(x, y);
+  // printf("Z2 = %.2f \n", z2);
+  // z3 = proizvod(x,y) + oduzimanje(5, y);
+  // printf("Z3 = %.2f \n", z3);
 
-  printf("Proizvod ova dva uneta broja je: %.2f \n", proizvod_funkcija);
-  printf("Zbir ova dva uneta broja je: %.2f \n", zbir_funkcija);
-  printf("Kolicnik ova dva uneta broja je: %.2f \n", deljenje_funkcija);
-  printf("Razlika ova dva uneta broja je: %.2f \n", oduzimanje_funkcija);
+  // float proizvod_funkcija = proizvod(a, b);
+  // float zbir_funkcija = zbir(a, b);
+  // float deljenje_funkcija = deljenje(a, b);
+  // float oduzimanje_funkcija = oduzimanje(a, b);
+
+  // printf("Proizvod ova dva uneta broja je: %.2f \n", proizvod_funkcija);
+  // printf("Zbir ova dva uneta broja je: %.2f \n", zbir_funkcija);
+  // printf("Kolicnik ova dva uneta broja je: %.2f \n", deljenje_funkcija);
+  // printf("Razlika ova dva uneta broja je: %.2f \n", oduzimanje_funkcija);
   // int a, b, c, d;
   // printf("a = \n");
   // scanf("%d", &a);
